3597dc801286dcec35abbd7e1d293c2264a35f61
Hoangh ANh <hatran22@colby.edu>
2021-11-16 20:08:10 -0500

    done with 1 ext

--- before/class_based_pong_before.py
+++ before/class_based_pong_before.py
@@ -12,13 +12,13 @@ import turtle
 class Paddle:
     # implements a Pong game paddle
 
-    def __init__(self, x_position, y_position):
+    def __init__(self, turtle,x_position, y_position):
         ''' initializes a paddle with a position '''
 
         self.x_position = x_position
         self.y_position = y_position
 
-        self.turt = make_turtle("square", "white", 5, 1, self.x_position, self.y_position)
+        self.turt = turtle
 
 
     def up(self):
@@ -114,17 +114,6 @@ def make_window(window_title, bgcolor, width, height):
     return window
 
 
-def make_turtle(shape, color, stretch_width, stretch_length, x_pos, y_pos):
-    ''' creates a turtle and sets initial position '''
-
-    turt = turtle.Turtle()
-    turt.speed(0) # Speed of animation, 0 is max
-    turt.shape(shape) # square defualt is 20,20
-    turt.color(color)
-    turt.shapesize(stretch_width, stretch_length) 
-    turt.penup()
-    turt.goto(x_pos, y_pos) #Start position
-    return turt
 
 
 def main():
